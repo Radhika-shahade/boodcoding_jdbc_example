0d957a8e874178eab3bbbe2120e0c94384d0f287
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-20 18:46:14 +0530

    jdbc task

---
+++ src/main/java/bootcoding/jdbc/hospital/Patient.java
@@ -0,0 +1,91 @@
+package bootcoding.jdbc.hospital;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Patient {
+    int id;
+    String name;
+    String city;
+    String disease;
+    int mobile_number;
+    String email_id;
+ Date admission_date;
+ Date discharge_date;
+
+    public Date getAdmission_date() {
+        return admission_date;
+    }
+
+    public void setAdmission_date(Date admission_date) {
+        this.admission_date = admission_date;
+    }
+
+    public Date getDischarge_date() {
+        return discharge_date;
+    }
+
+    public void setDischarge_date(Date discharge_date) {
+        this.discharge_date = discharge_date;
+    }
+
+    boolean is_cure;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    public int getMobile_number() {
+        return mobile_number;
+    }
+
+    public void setMobile_number(int mobile_number) {
+        this.mobile_number = mobile_number;
+    }
+
+    public String getEmail_id() {
+        return email_id;
+    }
+
+    public void setEmail_id(String email_id) {
+        this.email_id = email_id;
+    }
+
+
+    public boolean isIs_cure() {
+        return is_cure;
+    }
+
+    public void setIs_cure(boolean is_cure) {
+        this.is_cure = is_cure;
+    }
+}
