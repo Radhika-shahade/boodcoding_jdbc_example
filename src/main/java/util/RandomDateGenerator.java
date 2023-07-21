0316f9587c6f901c2396275607735e1a7dcc89fb
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-21 10:12:12 +0530

    jdbc task

---
+++ src/main/java/util/RandomDateGenerator.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+public class RandomDateGenerator {
+public  Date getDate()
+    {
+        Random random= new Random();
+        Calendar calender = Calendar.getInstance();
+        Date now = new Date();
+        int daysToSubstract = -1 * random.nextInt(2 * 365);
+        calender.setTime(now);
+        calender.add(Calendar.DAY_OF_YEAR, daysToSubstract);
+        Date yesterday = calender.getTime();
+        return yesterday;
+
+    }
+}
