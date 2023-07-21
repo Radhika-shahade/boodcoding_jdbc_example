61bdba48d1eb343bc708a61158dc0616a8b76691
Radhika shahade <radhikashahade0804@gmail.com>
2023-07-21 10:10:31 +0530

    jdbc task

--- src/main/java/bootcoding/jdbc/hospital/DaoService.java
+++ src/main/java/bootcoding/jdbc/hospital/DaoService.java
@@ -17,25 +17,28 @@ public class DaoService {
             Class.forName("org.postgresql.Driver");
             for(int i=0;i< data.size();i++) {
                 connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Admin");
-                psmt = connection.prepareStatement("insert into hospitaldb values (?,?,?,?,?,?)");
+                psmt = connection.prepareStatement("insert into hospitaldb values (?,?,?,?,?,?,?,?,?)");
                     Patient value= data.get(i);
                     psmt.setInt(1,value.getId());
-                    psmt.setString(2, value.getName());
-                psmt.setString(3,value.getDisease());
-                psmt.setInt(4,value.getMobile_number());
+                    psmt.setString(2, value.getName());psmt.setString(3,value.getDisease());
+                    psmt.setInt(4,value.getMobile_number());
                     psmt.setString(5, value.getCity());
                     psmt.setString(6,value.getEmail_id());
-//                    psmt.setBoolean(7,value.is_cure);
+                    java.util.Date utilDate = value.getAdmission_date();
+                    java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+                    psmt.setDate(7, sqlDate);
+                    java.util.Date utilDate2 = value.getDischarge_date();
+                   java.sql.Date sqlDate2 = new java.sql.Date(utilDate.getTime());
+                   psmt.setDate(8, sqlDate);
+                    psmt.setBoolean(9,value.is_cure);
                     int insert=psmt.executeUpdate();
                     if(insert==1)
                     {
-                        System.out.println(insert +" record inserted");
+                        System.out.println(i +" record inserted");
                     }
                     else {
                         System.out.println("failed to insert record");
                     }
-
-
           }
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
