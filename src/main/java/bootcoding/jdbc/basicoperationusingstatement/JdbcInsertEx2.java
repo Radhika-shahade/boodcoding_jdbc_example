3152b2fad5493b5c3eac15ee2aa563754e68f3c1
Radhika shahade <radhikashahade0804@gmail.com>
2023-09-13 23:12:28 +0530

    jdbc task

--- src/main/java/bootcoding/jdbc/basicoperationusingstatement/JdbcInsertEx2.java
+++ src/main/java/bootcoding/jdbc/basicoperationusingstatement/JdbcInsertEx2.java
@@ -8,26 +8,22 @@ import java.sql.Statement;
 public class JdbcInsertEx2 {
 
     public static void main(String[] args) {
-        Connection connection=null;
-        Statement statement=null;
-       try {
+        Connection connection = null;
+        Statement statement = null;
+        try {
             //load and register the driver
             Class.forName("org.postgresql.Driver");
             //establish the connection
-            connection= DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","Admin");
-           //create statement object
-             statement= connection.createStatement();
-           String sql="insert into students values (102,'ramesh','nagpur','civil')";
-           int n= statement.executeUpdate(sql);
-           System.out.println("Record inserted: "+ n);
-        }
-        catch (ClassNotFoundException e)
-        {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "Admin");
+            //create statement object
+            statement = connection.createStatement();
+            String sql = "insert into students values (102,'ramesh','nagpur','civil')";
+            int n = statement.executeUpdate(sql);
+            System.out.println("Record inserted: " + n);
+        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
 
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
